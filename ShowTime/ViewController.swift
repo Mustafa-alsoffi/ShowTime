0cb39620db5aece0b7af2bfced836fb0b4d8ba74
MustafaSwift <alsoffimustafa@gmail.com>
2019-04-13 17:38:12 +0800

    Initial Commit

---
+++ ShowTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowTime
+//
+//  Created by Mustafa Alsoffi on 13/04/2019.
+//  Copyright © 2019 Mustafa Alsoffi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
